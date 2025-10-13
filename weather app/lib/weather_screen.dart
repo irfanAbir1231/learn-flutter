b3f221c135e33acaa5e5499761e0658d09b44883
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-10-13 11:58:39 +0600

    initial weather app

---
+++ weather app/lib/weather_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Weather app',
+          style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+        centerTitle: true,
+        actions: [
+          GestureDetector(child: Icon(Icons.refresh),),
+        ],
+      ),
+    );
+  }
+
+}
