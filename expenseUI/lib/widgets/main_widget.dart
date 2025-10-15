106488b57932910dd81afd7810a43b2055504a05
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-10-16 00:13:56 +0600

    making main widgets

--- expenseUI/lib/widgets/main_widget.dart
+++ expenseUI/lib/widgets/main_widget.dart
@@ -5,15 +5,159 @@ class MainWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: SizedBox(
-        height: 120,
-        child: Placeholder(
-          color: Colors.pink,
-          strokeWidth: 2,
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton.icon(
+              icon: Image.asset('images/send_money.jpg', width: 30 , height: 30,),
+            label: Text('', style: TextStyle(
+              //button e label add korle sidewise ashe
+              fontSize: 10,
+            ),),
+            onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/mobile_recharge.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/cash_out.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/make_payment.jpg',width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+          ],
+
         ),
-      ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton.icon(
+              icon: Image.asset('images/add_money.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/pay_bill.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/savings.png', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/loan.png', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                // first e container diye try korsilam but oita to button na ejonno kete disi
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton.icon(
+              icon: Image.asset('images/send_money.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/mobile_recharge.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/cash_out.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/make_payment.jpg',width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+          ],
+
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton.icon(
+              icon: Image.asset('images/add_money.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/pay_bill.jpg', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/savings.png', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+            ElevatedButton.icon(
+              icon: Image.asset('images/loan.png', width: 30 , height: 30,),
+              label: Text('', style: TextStyle(
+                //button e label add korle sidewise ashe
+                // first e container diye try korsilam but oita to button na ejonno kete disi
+                fontSize: 10,
+              ),),
+              onPressed: () => {},
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
